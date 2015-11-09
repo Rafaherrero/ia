ae07de0e13837edc260ae0c7dd601079fec9e949
Daniel Ramos <danielramosacosta@hotmail.com>
2015-11-09 12:05:52 +0000

    Terminada documentación harry

--- Implementación Gráfica 1/hp_mdl/harryPotter.h
+++ Implementación Gráfica 1/hp_mdl/harryPotter.h
@@ -35,6 +35,10 @@ private:
 	unsigned costo_transicion_; //Coste de los nodos
 	bool tipo_distancia_; //falso = manhattan, true = euclides
 
+	unsigned funcion_heuristica_prox(QPoint);// Función de estimación heurística por proximidad al objetivo
+	QPoint get_next_dir_DFS(); //Metodo para obtener el siguiente paso del DFS
+	QPoint get_next_dir_LRTA(); //Método para obtener el siguiente paso del LRTA*
+	bool estoy_en_la_copa(); //Metodo para saber si estamos situados sobre la copa
 public:
 	/// \name Constructores
 	/// @{
@@ -45,33 +49,89 @@ public:
 	harryPotter(mapa_t& lab);
 	///@}
 
+	/// \name Accesores a Harry
+	/// @{
+	/*!
+	 * \brief Método para obetener la posición de Harry
+	 * \return El Punto en el que se encuentra Harry
+	 */
 	QPoint get_posicion_harry();
+
+	/*!
+	 * \brief Método para cambiar la posición de Harry
+	 * \param pos Posición a la que se desea cambiar
+	 */
 	void set_posicion_harry(QPoint pos);
+
+	/*!
+	 * \brief Además de cambiar la posición, este método resetea todo el laberinto y hace una limpieza
+	 * \param nueva_posicion Nueva posición de Harry.
+	 */
 	void set_posicion_harry_nuevo (QPoint nueva_posicion);
+	///@}
 
-	bool estoy_en_la_copa();
+	/// \name Modifición de parámetros de los Algoritmos
+	///@{
+
+	/*!
+	 * \brief Se usa para modificar el coste de la transición entre los nodos
+	 * \return Una referencia al coste
+	 */
 	unsigned& costo_transicion(void);
+
+	/*!
+	 * \brief Con esto se cambia el tipo de distancia, entre manhattan o eculides
+	 * \return Una referencia al valor booleano, false=manhattan, true=euclides
+	 */
 	bool& tipo_distancia(void);
+	///@}
 
+	/// \name Algoritmos disponibles
+	/// @{
+	/*!
+	 * \brief Analiza todas las condiciones de parada del DFS
+	 * \return Si es falso Harry muere o encuentra la copa, en otro caso continua la ejecución
+	 */
 	bool puedo_continuar_DFS();
+
+	/*!
+	 * \brief Ejecuta una iteración del algoritmo DFS
+	 * \return Devuelve la posicion actual de Harry
+	 */
 	QPoint movimiento_DFS();
-	QPoint get_next_dir_DFS();
 
-
-	unsigned funcion_heuristica_prox(QPoint);
-
+	/*!
+	 * \brief Analiza todas las condiciones de parada del A*
+	 * \return Si es falso Harry muere o encuentra la copa, en otro caso continua la ejecución
+	 */
 	bool puedo_continuar_estrella();
+
+	/*!
+	 * \brief Ejecuta una iteración del algoritmo A*
+	 * \return Devuelve la posicion actual de Harry
+	 */
 	QPoint movimiento_estrella();
 
+	/*!
+	 * \brief Analiza todas las condiciones de parada del LRTA*
+	 * \return Si es falso Harry muere o encuentra la copa, en otro caso continua la ejecución
+	 */
 	bool puedo_continuar_LRTA();
-	QPoint movimiento_LRTA();
-	QPoint get_next_dir_LRTA();
 
+	/*!
+	 * \brief Ejecuta una iteración del algoritmo LRTA*
+	 * \return Devuelve la posicion actual de Harry
+	 */
+	QPoint movimiento_LRTA();
+	///@}
+
+	/// \name Métodos en deshuso
+	///@{
 	unsigned get_mana();
 	unsigned get_vidas();
 	void set_mana(unsigned mana_encontrado);
 	void set_vidas(unsigned vida_gastada);
-
+	///@}
 };
 
 #endif // harryPotter_H
